68f0e5a9e6bc1ce06c562f6495d712fd418f7594
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-02-02 21:06:26 +0000

    basic API call from europeana

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,22 @@
-//hello javaScript for ArTrek, plz be kind
+
+
+const APIKey =  "tatityas";
+
+// search term variable that needs to come from searhc bar
+var searchTerm = "impressionism";
+
+
+var queryURL = "https://api.europeana.eu/record/v2/search.json?wskey=" + APIKey + "&query=" + searchTerm;
+
+
+
+// return data from the search term
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response.items);
+  });
+
+
+
