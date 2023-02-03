e7c8a0d7287559800cc1c8e2154a8adda08005ce
Jakinlade <jj.47.johnson@gmail.com>
2023-02-03 15:40:03 +0000

    Search input now runs through the Europeana API and returns 12 items

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,8 +6,19 @@ var EUAPIKey =  "tatityas";
 var GGAPIKey = "AIzaSyADX0Zd0LOToDJv9h85TLZXc8GUliRPzTU";
 
 
-// create a function to search EU API when keyword is entered into search bar
-  // select specific data from the 12 returned objects
+   // create a function to search EU API when keyword is entered into search bar
+function searchEuropeana(event) {
+  event.preventDefault();
+
+  var EUQuery = $("#userInput").val(); 
+  console.log(EUQuery);
+  var queryURL = "https://api.europeana.eu/record/v2/search.json?wskey=" + EUAPIKey + "&query=" + EUQuery;
+  // log the data available on input of a keyword
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // select specific data from the 12 returned objects
   // store results for each item together in a new object for each item
     // Select Title *
     // Select thumbnail image *
@@ -17,26 +28,14 @@ var GGAPIKey = "AIzaSyADX0Zd0LOToDJv9h85TLZXc8GUliRPzTU";
     // select location
       // Latitude
       // Longitude
-
-  // display * info of new simplified item objects in small cards on main page
-
-  // on clicking on card, display a modal which shows the more detailed info of the whole object
-    //Display a map with marker in this modal
-      // use the objects lat and long data as input for the Maps API to display Marker
-  
-function searchEuropeana(event) {
-  event.preventDefault();
-
-  var EUQuery = document.getElementById("userInput").value;
-  console.log(EUQuery);
-  var queryURL = "https://api.europeana.eu/record/v2/search.json?wskey=" + EUAPIKey + "&query=" + EUQuery;
-  // log the data available on input of a city name
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
       console.log(response);
     });
 }
 
 searchFormEl.on('submit', searchEuropeana);
+
+ // display * info of new simplified item objects in small cards on main page
+
+  // on clicking on card, display a modal which shows the more detailed info of the whole object
+    //Display a map with marker in this modal
+      // use the objects lat and long data as input for the Maps API to display Marker
